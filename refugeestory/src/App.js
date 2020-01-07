f65cae3610abb98bacb0035f7240eb532692c3b4
Dave Betts <ibedave777@gmail.com>
2020-01-06 23:09:30 -0500

    removed login from app.js as it will be its own page

--- refugeestory/src/App.js
+++ refugeestory/src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HomeCarousel from './components/Carousel'
-import Login from './components/Login'
+
 
 function App() {
   return (
     <div className="App">
-      <Login />
       <HomeCarousel />
     </div>
   );
