de9c9ddd10231f568b98ad1fd9916eabf9b74b49
Nicole white <you@example.com>
2020-01-07 07:46:15 -1000

    card frames in, api axios call in app.js

--- refugeestory/src/App.js
+++ refugeestory/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Route} from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
@@ -6,17 +6,28 @@ import HomeCarousel from './components/Carousel';
 import Login from './components/Login';
 import Nav from './components/Nav';
 import SubmitStory from './components/SubmitStory';
-
+import StoryCard from './components/StoryCard';
+import StoryReviewCard from './components/PendingStoryCards';
+import { axiosWithAuth } from './axiosWithAuth';
+import axios from 'axios';
 
 
 function App() {
+  const [story, setStory] = useState([]);
+  useEffect(() => {
+    axios.get('https://bw-refugee-stories-2.herokuapp.com/api/stories')
+    .then(res => {setStory(res.data.results)}, console.log(setStory))
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
       <Nav />
       <Route exact path ="/"/>
       <Route path="/login"><Login/></Route>
-      <Route path="/Submit"><SubmitStory/></Route>
+      <Route path="/Submit"><SubmitStory/></Route>      
       <HomeCarousel />
+      <StoryReviewCard />
+      <StoryCard />
     </div>
   );
 }
